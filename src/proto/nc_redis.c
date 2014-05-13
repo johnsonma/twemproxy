3a42b311d03a5e04f91c81602adfaa092d4abdf5
johnson <johnson@topcmm.com>
2014-05-13 17:07:05 +0800

    fix the code
    Change-Id: I2a7437efc5fa6ccd768d134378e10450317d0044

--- src/proto/nc_redis.c
+++ src/proto/nc_redis.c
@@ -456,6 +456,7 @@ redis_parse_req(struct msg *r)
                     break;
                 }
                 if (str4icmp(m, 'q', 'u', 'i', 't')) {
+                    r->type = MSG_REQ_REDIS_QUIT;
                     r->quit = 1;
                     break;
                 }
